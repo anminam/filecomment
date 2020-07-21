f6d80de316876a2800babcce5d3726a2c5f4eeec
anminam <minamhunmin@gmail.com>
2020-07-21 20:36:06 +0900

    update

--- app.js
+++ app.js
@@ -2,4 +2,18 @@ const { start } = require("./module/fileCommnet");
 
 const BASE_PATH = "/Users/anminam";
 
-start(BASE_PATH, "함수이름", ".js", `// 주석`);
+const obj = {
+  dir: BASE_PATH,
+  filter: "\tsetValue: function(inKey, inValue) {",
+  ext: ".js",
+  addStr: `\t\t/**
+\t\t* Data 값 저장.
+\t\t* @method setValue
+\t\t* @param  {String}    inKey   저장 키.
+\t\t* @param  {any}       inValue 저장 값.
+\t\t*/
+`,
+  fileFilters: ["Data.js"],
+};
+
+start(obj);
--- module/fileCommnet.js
+++ module/fileCommnet.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 
+const removeDirs = ["node_modules"];
+
 /**
  * 파일 찾아서 검색
  * @param {*} dir path
@@ -8,7 +10,7 @@ const fs = require("fs");
  * @param {*} ext 확장자
  * @return {string[]} 찾은 파일리스트
  */
-const searchFilesInDirectory = (dir, filter, ext) => {
+const searchFilesInDirectory = (dir, filter, ext, fileFilters) => {
   if (!fs.existsSync(dir)) {
     console.log(`Specified directory: ${dir} does not exist`);
     return;
@@ -16,21 +18,38 @@ const searchFilesInDirectory = (dir, filter, ext) => {
 
   const findFiles = [];
 
-  const files = getFilesInDirectory(dir, ext);
-  files.forEach((file) => {
+  const files = getFilesInDirectory(dir, ext, fileFilters);
+  files.forEach((file, i) => {
+    if (file === undefined) {
+      return true;
+    }
     const fileContent = fs.readFileSync(file);
 
-    // We want full words, so we use full word boundary in regex.
-    const regex = new RegExp("\\b" + filter + "\\b");
+    let newFilter = 특수문자추가기(filter);
+    const regex = new RegExp(`\t?${newFilter}`);
     if (regex.test(fileContent)) {
       findFiles.push(file);
-      console.log(`success ${file}`);
+      console.log("찾기", "success", ` ${i} ${file}`);
     } else {
-      console.log(`fail ${file}`);
+      // console.log("찾기", "fail", ` ${i} ${file}`);
     }
   });
 
   return findFiles;
+};
+
+const 특수문자추가기 = (str) => {
+  let tempArr = str.split("");
+  tempArr = tempArr.map((value) => {
+    if (["(", ")", "{", "}"].includes(value)) {
+      value = "\\" + value;
+    } else if (" ".includes(value)) {
+      value = "\\s?";
+    }
+    return value;
+  });
+
+  return tempArr.join("");
 };
 
 /**
@@ -39,22 +58,50 @@ const searchFilesInDirectory = (dir, filter, ext) => {
  * @param {*} ext
  * @return {string[]} files
  */
-const getFilesInDirectory = (dir, ext) => {
+const getFilesInDirectory = (dir, ext, fileFilters) => {
   if (!fs.existsSync(dir)) {
     console.log(`Specified directory: $ {dir}does not exist `);
     return;
   }
 
+  if (removeDirs) {
+    let isPass = false;
+    removeDirs.forEach((iDir) => {
+      if (dir.includes(iDir)) {
+        isPass = true;
+      }
+    });
+    if (isPass) {
+      return;
+    }
+  }
+
   let files = [];
-  fs.readdirSync(dir).forEach((file) => {
+  fs.readdirSync(dir).forEach((file, i) => {
+    // . @ 로 시작하는 파일 제거
+    if ([".", "@"].includes(file.substr(0, 1))) {
+      return true;
+    }
+
     const filePath = path.join(dir, file);
     const stat = fs.lstatSync(filePath);
 
-    // If we hit a directory, apply our function to that dir. If we hit a file, add it to the array of files.
+    // 디랙토리면
     if (stat.isDirectory()) {
-      const nestedFiles = getFilesInDirectory(filePath, ext);
+      const nestedFiles = getFilesInDirectory(filePath, ext, fileFilters);
       files = files.concat(nestedFiles);
+      if (i === 36) {
+        i;
+      }
+
+      // 파일이면
     } else {
+      // 파일 필터가 있으면 필터 있는것만 사용
+      if (fileFilters) {
+        if (!fileFilters.includes(file)) {
+          return true;
+        }
+      }
       if (path.extname(file) === ext) {
         files.push(filePath);
       }
@@ -78,6 +125,9 @@ const changeStr = (targetStr, filterStr, addStr) => {
 
   const exp = new RegExp(/\/\*[\s\S]*?\*\/|\/\/.*/g);
   const arr = [...front.matchAll(exp)];
+  if (arr.length === 0) {
+    return targetStr;
+  }
   const poped = arr.pop();
 
   // TODO: 함수 위에있는 것이 주석인지 다른 변수나, 함수인지 판단해서 지울지 말지 정하는것 추가해야함
@@ -98,20 +148,45 @@ const changeStr = (targetStr, filterStr, addStr) => {
 
 /**
  * 시작
- * @param {*} dir
- * @param {*} filter
- * @param {*} ext
- * @param {*} addText
+ * @param {string} dir 경로
+ * @param {string} filter 찾을문자
+ * @param {string} ext 확장명
+ * @param {string} addStr  추가할 문자
+ * @param {string[]} fileFilters 필터 파일 배열 (확장자 포함)
  */
-const start = (dir, filter, ext, addText) => {
-  const files = searchFilesInDirectory(dir, filter, ext);
-  files.forEach((file) => {
-    const fileContent = fs.readFileSync(file);
-    const changedStr = changeStr(fileContent.toString(), filter, addText);
-    fs.writeFile(file, changedStr, "utf-8", (err) => {
-      console.log(`${err}, 파일을 쓸 수없습니다.`);
-    });
-  });
+const start = async ({ dir, filter, ext, addStr, fileFilters }) => {
+  const files = searchFilesInDirectory(dir, filter, ext, fileFilters);
+  for (file of files) {
+    try {
+      const fileContent = await fs.readFileSync(file);
+      const changedStr = changeStr(fileContent.toString(), filter, addStr);
+      console.log(file, "쓰는중");
+      await fs.writeFileSync(file, changedStr, "utf-8");
+      console.log(file, "쓰기완료");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  // await files.forEach(async (file) => {
+  //   try {
+  //     const fileContent = await fs.readFileSync(file);
+  //     const changedStr = changeStr(fileContent.toString(), filter, addStr);
+  //     console.log(file, "쓰는중");
+  //     await fs.writeFileSync(file, changedStr, "utf-8");
+  //     console.log(file, "쓰기완료");
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+
+  //   // fs.writeFile(file, changedStr, "utf-8", (err) => {
+  //   //   if (err) {
+  //   //     console.log(`${err}, 파일을 쓸 수없습니다.`);
+  //   //   }
+  //   //   console.log("파일쓰기 성공");
+  //   // });
+  // });
+
+  console.log("종료");
 };
 
 module.exports = {
