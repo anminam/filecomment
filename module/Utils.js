dd019ec605723f1db935b13d4504075a2cdcb8ad
anminam <minamhunmin@gmail.com>
2020-08-05 16:51:38 +0900

    update utils

--- module/Utils.js
+++ module/Utils.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 
 const Utils = {
   /**
@@ -6,8 +7,8 @@ const Utils = {
    * @param {string} str 바꿀 문자열
    */
   addEscapeStr: (str) => {
-    let tempArr = str.split("");
-    tempArr = tempArr.map((value) => {
+    let strArr = str.split("");
+    strArr = strArr.map((value) => {
       if (["(", ")", "{", "}", "[", "]"].includes(value)) {
         value = "\\" + value;
       } else if (" ".includes(value)) {
@@ -16,17 +17,20 @@ const Utils = {
       return value;
     });
 
-    return tempArr.join("");
+    return strArr.join("");
   },
 
   /**
    * 파일 찾아서 검색
-   * @param {*} dir path
-   * @param {*} filter 필터
-   * @param {*} ext 확장자
+   * @param {Object} options
+   * @option {string} dir 경로
+   * @option {string} findStr 찾을문자
+   * @option {string} filter 필터
+   * @option {string} ext 확장자
+   * @option {Object} config object
    * @return {string[]} 찾은 파일리스트
    */
-  searchFilesInDirectory: (dir, filter, ext, fileFilters) => {
+  searchFilesInDirectory: ({ dir, findStr, ext, fileFilters, config }) => {
     const findFiles = [];
 
     if (!fs.existsSync(dir)) {
@@ -34,14 +38,14 @@ const Utils = {
       return [];
     }
 
-    const files = getFilesInDirectory(dir, ext, fileFilters);
+    const files = Utils.getFilesInDirectory({ dir, ext, fileFilters, config });
     files.forEach((file, i) => {
       if (file === undefined) {
         return true;
       }
       const fileContent = fs.readFileSync(file);
 
-      if (this.isFindedFile(fileContent, filter)) {
+      if (Utils.isFindedFile(fileContent, findStr)) {
         console.log(config.logPreText, "파일검색", "success", ` ${i} ${file}`);
         findFiles.push(file);
       }
@@ -57,7 +61,7 @@ const Utils = {
    * @return {boolean}
    */
   isFindedFile: (src, filter) => {
-    let newFilter = this.addEscapeStr(filter);
+    let newFilter = Utils.addEscapeStr(filter);
     const regex = new RegExp(`\t?${newFilter}`);
     if (regex.test(src)) {
       const regex2 = new RegExp("//[\\s]*" + newFilter);
@@ -74,19 +78,22 @@ const Utils = {
 
   /**
    * 디렉토리 안에 파일찾기
-   * @param {string} dir dir
-   * @param {string} ext 확장자
-   * @return {string[]} files 파일명 리스트
+   * @param {Object} options
+   * @option {Object} config object
+   * @option {string} dir 경로
+   * @option {string} ext 확장자
+   * @option {string[]} fileFilters 파일명 리스트
+   * @return {string[]} 찾은 file 리스트
    */
-  getFilesInDirectory: (dir, ext, fileFilters) => {
+  getFilesInDirectory: ({ config, dir, ext, fileFilters }) => {
     let files = [];
 
     if (!fs.existsSync(dir)) {
-      console.log(`Specified directory: $ {dir}does not exist `);
+      console.log(`Specified directory: ${dir} does not exist `);
       return files;
     }
 
-    if (config.removeDirs) {
+    if (config && config.removeDirs) {
       let isPass = false;
       config.removeDirs.forEach((tempDir) => {
         if (dir.includes(tempDir)) {
@@ -100,21 +107,22 @@ const Utils = {
 
     fs.readdirSync(dir).forEach((file, i) => {
       // . @ 로 시작하는 파일 제거
-      if (config.removePreNames.includes(file.substr(0, 1))) {
+      if (config && config.removePreNames.includes(file.substr(0, 1))) {
         return true;
       }
 
       const filePath = path.join(dir, file);
-      const stat = fs.lstatSync(filePath);
+      const stats = fs.lstatSync(filePath);
 
       // 디랙토리면
-      if (stat.isDirectory()) {
+      if (stats.isDirectory()) {
         // 파일 찾을때까지 재귀
-        const nestedFiles = this.getFilesInDirectory(
-          filePath,
+        const nestedFiles = Utils.getFilesInDirectory({
+          config: config,
+          dir: filePath,
           ext,
-          fileFilters
-        );
+          fileFilters,
+        });
         files = files.concat(nestedFiles);
 
         // 파일이면
